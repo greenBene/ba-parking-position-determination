dd98b4bb5c36242e7699c6bc13486964499d98f5
Benedikt Beckermann <benedikt.beckermann@kolibrigames.com>
2019-09-13 17:23:43 +0200

    Initial Commit

---
+++ ba-parking-position-determination/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ba-parking-position-determination
+//
+//  Created by Benedikt Beckermann on 13/09/2019.
+//  Copyright © 2019 Benedikt Beckermann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
