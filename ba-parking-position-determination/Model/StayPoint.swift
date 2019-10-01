63a581c1defd1bba55127feb97a90740ce5f110d
Benedikt Maria Beckermann <hello@benedikt.click>
2019-10-01 12:41:19 +0200

    added staypoint detection algorithm

---
+++ ba-parking-position-determination/Model/StayPoint.swift
@@ -0,0 +1,9 @@
+//
+//  StayPoint.swift
+//  ba-parking-position-determination
+//
+//  Created by Benedikt Beckermann on 01/10/2019.
+//  Copyright © 2019 Benedikt Beckermann. All rights reserved.
+//
+
+import Foundation
